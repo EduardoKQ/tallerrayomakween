539a0dfa298eabf758a799b60eaee59799e37d94
Eduardo Kusar <21264573+EduardoKQ@users.noreply.github.com>
2024-05-01 19:08:02 -0400

    agrega script para aumentar contador al hacer click en "agregar"

---
+++ src/scripts/contadorCarrito.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  // para probar borramos la data persistente
+  sessionStorage.removeItem("numProductos");
+});
+
+$(".btn-agregar-producto").click(function () {
+  agregarProducto();
+  actualizarContador();
+});
+
+function agregarProducto() {
+  let numProductos = sessionStorage.getItem("numProductos");
+
+  if (numProductos == null) {
+    numProductos = 1;
+  } else {
+    numProductos = parseInt(numProductos) + 1;
+  }
+
+  sessionStorage.setItem("numProductos", numProductos);
+}
+
+function actualizarContador() {
+  let numProductos = sessionStorage.getItem("numProductos");
+
+  $("#contadorCarrito").text(numProductos);
+
+  if (numProductos == 0) {
+    $("#contadorCarrito").css("display", "none");
+  } else {
+    $("#contadorCarrito").css("display", "block");
+  }
+}
